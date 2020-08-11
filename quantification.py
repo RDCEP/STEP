b7ea594a5905493e3d770da32118f1329524da0b
Alex Rittler <alexrittler@Alexs-MBP-4.fios-router.home>
2020-08-11 16:33:24 -0400

    first commit

---
+++ quantification.py
@@ -0,0 +1,238 @@
+from collections import defaultdict
+from math import atan, sqrt
+import numpy as np
+
+
+def quantify(tracked_storms: np.ndarray, precip_data: np.ndarray, lat_data: np.ndarray, long_data: np.ndarray,
+             time_interval: float, pixel_size: float) -> tuple:
+    """Quantitatively describes individual storms in terms of duration, size, mean intensity, and central location.
+    :param tracked_storms: the tracked storms returned by the tracking algorithm, given as an array of dimensions
+    Time x Rows x Cols.
+    :param precip_data: the precipitation data corresponding to the tracked storms data, with the same dimensions as
+    tracked_storms.
+    :param lat_data: the latitude data corresponding to each [y][x] in tracked_storms, given as an array of
+    dimensions 1 x Rows x Cols.
+    :param long_data: the longitude data corresponding to each [y][x] in tracked_storms, given as an array of
+    dimensions 1 x Rows x Cols.
+    :param time_interval: the period between temporal 'snapshots', given as a float. The user should interpret the
+    duration results in terms of the time unit implied here.
+    :param pixel_size: the length/width one grid cell represents in the data. The user should interpret the size and
+    average intensity results in terms of the distance unit implied here squared.
+    :return: A tuple of size four containing the duration of each storm, as well as its size, intensity,
+    and central location at each time step, in this order.
+    """
+
+    # find the duration of the storms
+    durations = get_duration(tracked_storms, time_interval)
+
+    # find the size of each storm in each time slice
+    sizes = get_size(tracked_storms, pixel_size)
+
+    # find the average precipitation amount for each storm in each time slice
+    averages = get_average(tracked_storms, precip_data)
+
+    # and find the central location for each storm in each time slice
+    central_locs = get_central_loc(tracked_storms, precip_data, lat_data, long_data)
+
+    return durations, sizes, averages, central_locs
+
+
+def get_duration(storms: np.ndarray, time_interval: float) -> np.ndarray:
+    """Computes the duration (in the time unit of time_interval) of each storm across all time slices given.
+    :param storms: the tracked storms returned by the tracking algorithm, given as an array of dimensions
+    Time x Rows x Cols.
+    :param time_interval: the period between temporal 'snapshots', given as a float.
+    :return: An array of length equal to the number of tracked storms + 1, where the value at [x] corresponds to
+    the duration of the storm x. The index 0 (referring to the background) is always 0 and provided for ease of
+    indexing.
+    """
+
+    # find the number of time slices in the data
+    lifetime = storms.shape[0]
+
+    # initialize a new dictionary
+    duration_dict = defaultdict(int)
+
+    # and the number of storms
+    total_storms = len(np.unique(storms))
+
+    print(total_storms)
+
+    # and an array to store the result in, where the value found at each index corresponds to the duration that storm
+    result = np.zeros(total_storms)
+
+    # then, for each time slice
+    for time_index in range(lifetime):
+        # compute the labels that appear in that time slice
+        curr_labels = np.unique(storms[time_index])
+
+        # for each label in the tracked storm data
+        for label in range(total_storms):
+            # if it appears in the current time slice
+            if label and np.isin(label, curr_labels):
+                # increment the number of time slices it appears in
+                # (and if we haven't seen it before, set it to 1 in the dictionary (this is a property of defaultdict)
+                duration_dict[label] += 1
+
+    for key, value in duration_dict.items():
+        if key:
+            result[key] = value
+
+    result = result * time_interval
+
+    return result
+
+
+def get_size(storms: np.ndarray, grid_cell_size: float) -> np.ndarray:
+    """Computes the size (in the distance unit of grid_cell_size) of each storm across all time slices given.
+    :param storms: the tracked storms returned by the tracking algorithm, given as an array of dimensions
+    Time x Rows x Cols.
+    :param grid_cell_size: the length/width one grid cell represents in the data, given as a float.
+    :return: a lifetime x total_storms array where the value found at [y][x] corresponds to the size of the storm at t=y,
+    storm=x. Except in the case of index 0, which is always 0 for any t.
+    """
+
+    # find the number of time slices in the data
+    lifetime = storms.shape[0]
+
+    # TODO: CHANGED TO LEN, NOT SURE HOW WORKING BEFORE
+    # and the number of storms
+    total_storms = len(np.unique(storms))
+
+    # initialize an array with dimensions number of time slices by number of storms
+    result = np.zeros((lifetime, total_storms))
+
+    for time_index in range(lifetime):
+        # find the unique labels
+        labels = np.unique(storms[time_index])
+
+        # for each label that appears in this time slice (that's not the background)
+        for label in labels:
+            if label:
+
+                # compute its number of grid cells using a map and reduce technique
+                storm_size = np.sum(np.where(storms[time_index] == label, 1, 0))
+
+                # and place it at that correct location in the array to return
+                result[time_index][label] = storm_size
+
+    # multiply the number of grid cells in each storm by the grid cell size
+    result = result * grid_cell_size
+
+    return result
+
+
+def get_average(storms: np.ndarray, precip: np.ndarray) -> np.ndarray:
+    """Computes the average intensity of each storm across all time slices given.
+    :param storms: the tracked storms returned by the tracking algorithm, given as an array of dimensions
+    Time x Rows x Cols.
+    :param precip: the precipitation data corresponding to the tracked storms, with the same dimensions as
+    tracked_storms.
+    :return: a lifetime x total_storms array where the value found at [y][x] corresponds to the mean intensity of the
+    storm at t=y, storm=x. Except in the case of index 0, which is always 0 for any t.
+    """
+
+    # find the number of time slices in the data
+    lifetime = storms.shape[0]
+
+    # and the number of storms
+    total_storms = len(np.unique(storms))
+
+    # initialize an array with dimensions number of time slices by number of storms
+    result = np.zeros((lifetime, total_storms))
+
+    for time_index in range(lifetime):
+        # find the unique labels
+        labels = np.unique(storms[time_index])
+
+        # for each label that appears in this time slice (that's not the background)
+        for label in labels:
+
+            if label:
+                # find the precipitation where it appears in the current time slice
+                storm_precip = np.where(storms[time_index] == label, precip[time_index], 0)
+
+                # sum the precipitation
+                storm_precip_sum = np.sum(storm_precip)
+
+                # find the number of grid cells belonging to the storm
+                storm_size = np.sum(np.where(storms[time_index] == label, 1, 0))
+
+                # find the storm's average precipitation in this time slice
+                storm_avg = storm_precip_sum / storm_size
+
+                # and store it in the appropriate place in our result array
+                result[time_index][label] = storm_avg
+
+    return result
+
+
+def get_central_loc(storms: np.ndarray, precip: np.ndarray, lats: np.ndarray, longs: np.ndarray) \
+        -> np.ndarray:
+    """Computes the central location on the earth's surface of each storm across all time slices given.
+    :param storms: the tracked storms returned by the tracking algorithm, given as an array of dimensions
+    Time x Rows x Cols.
+    :param precip: the precipitation data corresponding to the tracked storms data, with the same dimensions as
+    tracked_storms.
+    :param lats: The latitude data corresponding to each [y][x] in tracked_storms, given as an array of dimensions
+    1 x Rows x Cols.
+    :param longs: The longitude data corresponding to each [y][x] in tracked_storms, given as an array of dimensions
+    1 x Rows x Cols.
+    :param size_array: the array returned by get_size(), a lifetime x total_storms array where the value found at [y][x]
+    corresponds to the size of the storm at time=y, storm=x.
+    :param lifetime: the number of time slices in the data, given as an integer.
+    :param total_storms: the total number of storms INCLUDING the background, given as an integer.
+    :return: a lifetime x total_storms array where the value found at [y][x] corresponds to the central location of the
+    storm at t=y, storm=x. Except in the case of index 0, which is always 0 for any t.
+    """
+
+    lifetime = storms.shape[0]
+
+    total_storms = len(np.unique(storms))
+
+    # initialize an array to store our result, but of type object to allow us to store an array in each cell
+    result = np.zeros((lifetime, total_storms)).astype(object)
+
+    # create arrays of x, y, and z values for the cartesian grid in R3
+    x_array = np.cos(lats) * np.cos(longs)
+    y_array = np.cos(lats) * np.sin(longs)
+    z_array = np.sin(lats)
+
+    # create an array to hold each central location as we calculate it
+    central_location = np.empty(2)
+
+    for time_index in range(lifetime):
+        # find the unique labels
+        labels = np.unique(storms[time_index])
+
+        for label in labels:
+            # if the storm exists in this time slice
+            if label:
+
+                # find the sum of the precipitation values belonging to the storm
+                sum_precipitation = np.sum(np.where(storms[time_index] == label, precip[time_index], 0))
+
+                # and compute the intensity weighted averages
+                x_avg = np.sum(np.where(storms[time_index] == label, ((x_array[0] * precip[time_index]) /
+                                                                      sum_precipitation), 0))
+
+                y_avg = np.sum(np.where(storms[time_index] == label, ((y_array[0] * precip[time_index]) /
+                                                                      sum_precipitation), 0))
+
+                z_avg = np.sum(np.where(storms[time_index] == label, ((z_array[0] * precip[time_index]) /
+                                                                      sum_precipitation), 0))
+
+                h_avg = sqrt((x_avg ** 2) + (y_avg ** 2))
+                # print(f"CL before: {central_location}")
+
+                # the central location on earth's surface is given by the following
+                central_location[0] = 2 * atan(y_avg / (sqrt((y_avg ** 2) + (x_avg ** 2)) + x_avg))
+                central_location[1] = 2 * atan(z_avg / (sqrt((z_avg ** 2) + (h_avg ** 2)) + h_avg))
+
+                # and we place it in the appropriate spot in the array
+                result[time_index][label] = central_location
+
+                # reset the central location - this seems to be necessary here
+                central_location = np.zeros(2)
+
+    return result
---
+++ tracking.py
@@ -0,0 +1,261 @@
+import copy
+from math import sqrt
+import numpy as np
+from scipy.ndimage.measurements import center_of_mass
+from scipy.spatial.distance import pdist, squareform
+from skimage.segmentation import relabel_sequential
+
+
+def track(labeled_maps: np.ndarray, precip_data: np.ndarray, tau: float, phi: float, km: float,
+          test: bool = False) -> np.ndarray:
+    """Tracks rainstorm events over time by linking identified storms across consecutive time steps.
+    :param labeled_maps: the identified storms returned by the identification algorithm, given as an array of
+    dimensions Time x Rows x Cols.
+    :param precip_data: the raw precipitation data corresponding to the identified storms, with the same dimensions as
+    labeled_maps.
+    :param tau: the threshold at which a storm is considered similar enough to another to possibly be linked through
+    time, given as a float.
+    :param phi: the constant to be used in computing similarity between storms, given as a float.
+    :param km: the number of grid cells equivalent to 120km in the maps, given as a float.
+    :param test: turn on/off optional testing printouts to help tune parameters, given as a bool with default value
+    False.
+    :return: a Time x Rows x Cols array containing the identified storms, now tracked through time.
+    """
+
+    shape = labeled_maps.shape
+    num_time_slices = shape[0]
+
+    # make a copy of the result of the identification algorithm to avoid labeling collisions
+    # we will record any labeling changes here
+    result_data = copy.deepcopy(labeled_maps)
+
+    # skip labeling t=0, since it is already labeled correctly
+    # for every other time slice
+    for time_index in range(1, num_time_slices):
+        # find the labels for this time index and the labeled storms in the previous time index
+        current_labels = np.unique(labeled_maps[time_index])
+        previous_storms = np.unique(result_data[time_index - 1])
+
+        # and prepare the corresponding precipitation data
+        curr_precip_data = precip_data[time_index]
+        prev_precip_data = precip_data[time_index - 1]
+
+        # determine the maximum label already seen to avoid collisions
+        max_label_so_far = max(np.max(result_data[time_index - 1]), np.max(labeled_maps[time_index]))
+
+        # then, for each label in current time index (that isn't the background)
+        for label in current_labels:
+            if label:
+
+                # for testing only
+                if test:
+                    print(f'Label to match: {label} in time slice {time_index + 1}')
+
+                # make sure initially the max storm size and best matched storm are 0
+                max_size = 0
+                best_matched_storm = 0
+
+                # find where the labels of the current storm segment exist in the current time slice
+                current_label = np.where(labeled_maps[time_index] == label, 1, 0)
+
+                if test:
+                    print(f'Label size: {np.sum(current_label)}')
+
+                # find the precipitation data at those locations
+                curr_label_precip = np.where(labeled_maps[time_index] == label, curr_precip_data, 0)
+
+                # and its intensity weighted centroid
+                curr_centroid = center_of_mass(curr_label_precip)
+
+                # then for every labeled storm in the previous time index
+                for storm in previous_storms:
+
+                    if test:
+                        print(f'Possible match in previous time slice: {storm}')
+
+                    # find where the storm exists in the appropriate time slice
+                    previous_storm = np.where(result_data[time_index - 1] == storm, 1, 0)
+
+                    # compute the size of the previous storm
+                    prev_size = np.sum(previous_storm)
+
+                    if test:
+                        print(f'Possible match size: {prev_size}')
+
+                    # if the storm is not the background and the size of this storm is greater than that of the previous
+                    # best match
+                    if storm and prev_size > max_size:
+
+                        # if their similarity measure is greater than the set tau threshold
+                        if similarity(current_label, previous_storm, curr_precip_data, prev_precip_data, phi, test) \
+                                > tau:
+
+                            # find the precipitation data for this storm
+                            prev_storm_precip = np.where(result_data[time_index - 1] == storm, prev_precip_data, 0)
+
+                            # and its intensity-weighted centroid
+                            prev_centroid = center_of_mass(prev_storm_precip)
+
+
+                            curr_prev_displacement = displacement(curr_centroid, prev_centroid)
+                            curr_prev_magnitude = magnitude(curr_prev_displacement)
+
+                            if test:
+                                print(f'Current weighted centroid: {curr_centroid}')
+                                print(f'Previous weighted centroid: {prev_centroid}')
+                                print(f'Displacement: {curr_prev_displacement}')
+                                print(f'Vector magnitude: {curr_prev_magnitude}')
+
+                            # if the magnitude of their displacement vector is less than 120 km in grid cells
+                            if curr_prev_magnitude < km:
+                                if test:
+                                    print('Possible match through distance')
+
+                                # update the best matched storm information
+                                max_size = prev_size
+                                best_matched_storm = storm
+                            else:
+                                # otherwise, if the angle between this displacement vector and the previous displacement
+                                # vector associated with that label is less than 120 degrees and it's possible to find
+                                # this angle
+                                if time_index > 1:
+                                    predecessor_storms = np.unique(result_data[time_index - 2])
+                                    if np.isin(storm, predecessor_storms):
+
+                                        # compute the displacement between the possible match and its predecessor
+                                        predecessor_loc = np.where(result_data[time_index - 2] == storm, 1, 0)
+                                        pred_precip = np.where(predecessor_loc == 1, precip_data[time_index - 2], 0)
+                                        pred_centroid = center_of_mass(pred_precip)
+                                        prev_pred_displacement = displacement(prev_centroid, pred_centroid)
+                                        vec_angle = angle(curr_prev_displacement, prev_pred_displacement)
+                                else:
+                                    vec_angle = 1
+                                if test:
+                                    print(f'Angle: {vec_angle}')
+                                if vec_angle > -.33:    # equivalent to 120 degree direction difference
+                                    # update the best matched storm information
+                                    if test:
+                                        print('Possible match through angle')
+                                    max_size = prev_size
+                                    best_matched_storm = storm
+
+                # if we found matches
+                if max_size:
+                    # link the label in the current time slice with the appropriate storm label in the previous
+                    result_data[time_index] = np.where(labeled_maps[time_index] == label, best_matched_storm,
+                                                       result_data[time_index])
+                # otherwise we've detected a new storm
+                else:
+                    # give the storm a unique label
+                    result_data[time_index] = np.where(labeled_maps[time_index] == label, max_label_so_far + 1,
+                                                       result_data[time_index])
+                    # and increment the known maximum label to make sure we don't reuse a label
+                    max_label_so_far += 1
+
+                if test:
+                    print(f'{label} matched {best_matched_storm} in time slice {time_index + 1}')
+
+    # ensure that we've labeled the storms sequentially
+    # seq_result = np.empty_like(result_data)
+    seq_result = relabel_sequential(result_data)[0]
+
+    return seq_result
+
+
+def similarity(curr_label_locs: np.ndarray, prev_storm_locs: np.ndarray, curr_raw_data: np.ndarray,
+               prev_raw_data: np.ndarray, phi: float, test: bool) -> float:
+    """Computes the similarity measure between two storms.
+    :param curr_label_locs: the location of the storm in the current time slice, where 1 marks a cell that is
+    part of the storm and is 0 otherwise, given as an array of dimensions Rows x Cols.
+    :param prev_storm_locs: the location of the storm in the ~previous~ time slice, with the same type information as
+    curr_label_locs.
+    :param curr_raw_data: the raw precipitation data corresponding to the current time slice, with the same
+    type information as curr_label_locs.
+    :param prev_raw_data: the raw precipitation data corresponding to the previous time slice, with the same
+    type information as curr_label_locs.
+    :param phi: the constant to be used in computing similarity between storms, given as a float.
+    :param test: boolean to toggle testing printouts passed on from designation in track().
+    :return: the similarity measure of the two storms, as a float.
+    """
+
+    # for the similarity computation, it is strongly encouraged to draw out a small example to understand how this is
+    # being computed
+
+    # sum the precipitation data for both
+    curr_precip_sum = np.sum(np.where(curr_label_locs == 1, curr_raw_data, 0))
+    prev_precip_sum = np.sum(np.where(prev_storm_locs == 1, prev_raw_data, 0))
+
+    # find the weight of each cell in both
+    curr_weighted_locs = np.where(curr_label_locs == 1, curr_raw_data / curr_precip_sum, 0)
+    prev_weighted_locs = np.where(prev_storm_locs == 1, prev_raw_data / prev_precip_sum, 0)
+
+    # turn the label weighting for the current time slice into a 1d array
+    curr_coors = np.argwhere(curr_weighted_locs)
+
+    # do the same for the storm weightings in the previous time slice
+    prev_coors = np.argwhere(prev_weighted_locs)
+
+    # merge the two arrays
+    merged_coors = np.concatenate((curr_coors, prev_coors), axis=0)
+
+    # and find their union
+    union_helper = [tuple(row) for row in merged_coors]
+    union = np.unique(union_helper, axis=0)
+    # https://stackoverflow.com/a/31097302
+
+    # place the weights of these locations for each storm in its appropriate time slice (even if they include 0's)
+    curr_union_weights = np.zeros(len(union))
+    prev_union_weights = np.zeros(len(union))
+
+    for index, coors in enumerate(union):
+        curr_union_weights[index] = curr_weighted_locs[coors[0]][coors[1]]
+        prev_union_weights[index] = prev_weighted_locs[coors[0]][coors[1]]
+
+    # reshape these arrays to then perform matrix multiplication on them
+    curr_union_weights = curr_union_weights.reshape(curr_union_weights.shape[0], 1)
+    prev_union_weights = prev_union_weights.reshape(1, prev_union_weights.shape[0])
+
+    # compute their matrix multiplication as this gives the combinations of the desired weights multiplied
+    curr_prev_weights = np.einsum('ij, jk -> ik', curr_union_weights, prev_union_weights)
+    # https://stackoverflow.com/a/59858877
+
+    # find the corresponding distances to each combination cell in the array of multiplied weights
+    union_dists = squareform(pdist(union))
+
+    # and compute the exponential for each multiplied by the corresponding weights previously found
+    element_wise_similarity = (np.exp(-1 * phi * union_dists) * curr_prev_weights)
+
+    # the similarity measure is the sum of the all the cells in the this new array
+    similarity_measure = np.sum(element_wise_similarity)
+
+    if test:
+        print(f'Similarity measure: {similarity_measure}')
+
+    return similarity_measure
+
+
+def displacement(current: np.ndarray, previous: np.ndarray) -> np.array:
+    """Computes the displacement vector between the centroids of two storms.
+    :param current: the intensity-weighted centroid of the storm in the current time slice, given as a tuple.
+    :param previous: the intensity-weighted centroid of the storm in the previous time slice, given as a tuple.
+    :return: the displacement vector, as an array.
+    """
+    return np.array([current[0] - previous[0], current[1] - previous[1]])
+
+
+def magnitude(vector: np.ndarray) -> float:
+    """Computes the magnitude of a vector.
+    :param vector: the displacement vector, given as an array.
+    :return: its magnitude, as a float.
+    """
+    return sqrt((vector[0] ** 2) + (vector[1] ** 2))
+
+
+def angle(vec_one: np.ndarray, vec_two: np.ndarray) -> float:
+    """Computes the small angle between two vectors.
+    :param vec_one: the first vector, as an array.
+    :param vec_two: the second vector, as an array.
+    :return: the angle between them in degrees, as a float.
+    """
+    # for more information on the computation, https://gamedev.stackexchange.com/q/69475 may be helpful
+    return np.arccos(np.dot(vec_one, vec_two) / (np.linalg.norm(vec_one) * np.linalg.norm(vec_two)))
